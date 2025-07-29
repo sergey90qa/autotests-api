7c833186d88d805e152d68112cb15c4cccee7698
sergey90qa <sergey.yaroslavtsev.dev@yandex.ru>
2025-07-29 17:59:26 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def print_h1(name):
+    print(f"<h1>{name}</h1>")
+
+
+if __name__ == "__main__":
+    print_h1("Hello, World!")
+    print_h1("Welcome to Python Programming")
+    print_h1("This is a test header")
